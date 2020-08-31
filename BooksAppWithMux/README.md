806585f268c849874c1c337fad3b276a3a458338
Madhvi Patel <madhvi.patel@derivco.se>
2020-08-31 14:45:59 +0200

    updated readme and started timestamp project

---
+++ BooksAppWithMux/README.md
@@ -0,0 +1,11 @@
+App: BooksAppWithMux
+
+This is an simple book app  used for:
+
+Fetching  the list of books
+Fetch the book with id
+Create a book 
+Update a book
+Delete a book
+
+This application is using gorialla mux for the book webserver.
---
+++ timestamp/timeStamp.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func currentTimestamp(rw http.ResponseWriter, req *http.Request) {
+	t := time.Now()
+	rw.Write([]byte(t.String()))
+}
+
+func main() {
+	fmt.Println("Inside Main")
+	http.HandleFunc("/api/timestamp", currentTimestamp)
+	http.HandleFunc("/api/timestamp/date/{date}", timestamp)
+
+	http.ListenAndServe(":9000", nil)
+}
