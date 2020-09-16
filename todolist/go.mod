6e2083708475b73000b9fb576557e6b24dc4400f
Madhvi Patel <madhvi.patel@derivco.se>
2020-09-16 20:10:28 +0200

    adding go modules

---
+++ todolist/go.mod
@@ -0,0 +1,8 @@
+module github.com/pmadhvi/Golang/todolist
+
+go 1.14
+
+require (
+	github.com/lib/pq v1.8.0
+	github.com/sirupsen/logrus v1.6.0
+)
--- todolist/middleware/handlers.go
+++ todolist/middleware/handlers.go
@@ -47,7 +47,7 @@ func chkerr(err error) {
 }
 
 func HealthzHandler(w http.ResponseWriter, r *http.Request) {
-	log.Info("API Health is OK")
+	log.Info("API Health is OK------")
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, `{"alive": true}`)
 }
